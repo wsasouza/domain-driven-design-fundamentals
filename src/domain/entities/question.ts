7303ad47be1e8b773f6bfd4a4bbad2e738bd79e3
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-05-01 20:03:41 -0300

    Feat: Create Project and Structure

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,9 @@
+export class Question {
+  public title: string
+  public content: string
+
+  constructor(title: string, content: string){
+    this.title = title
+    this.content = content
+  }
+}
