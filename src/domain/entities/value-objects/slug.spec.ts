f6453f61bfbf5edfa18175c747868279c3d2407e
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-05-02 22:44:49 -0300

    Feat: Value Object Slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest"
+import { Slug } from "./slug"
+
+test('it should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example Question Title')
+
+  expect(slug.value).toEqual('example-question-title')
+})
+
