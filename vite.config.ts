c91a4a4d1c38638150e3524365e8717765ef5f47
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-05-05 16:05:01 -0300

    Feat: First part DDD

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite"
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths()
+  ],
+  test: {
+    globals: true,
+  }
+})
+
